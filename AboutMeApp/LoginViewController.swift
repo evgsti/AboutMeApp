8cd3ebf70f913b0fdd2726784807e12de86d8d67
Evgeniy <ev.shipitsyn@yandex.ru>
2024-07-27 17:54:19 +0300

    work with login screen logic

--- AboutMeApp/LoginViewController.swift
+++ AboutMeApp/LoginViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  LoginViewController.swift
 //  AboutMeApp
 //
 //  Created by Евгений on 26.07.2024.
@@ -7,18 +7,68 @@
 
 import UIKit
 
-class loginViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+final class LoginViewController: UIViewController {
+    
+    // MARK: - IB Outlets
+    @IBOutlet private var loginTextField: UITextField!
+    @IBOutlet private var passwordTextField: UITextField!
+    
+    // MARK: - Private Properties
+    private let userName = "swift"
+    private let password = "123"
+    
+    // MARK: - View Life Cycles
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        self.view.endEditing(true)
     }
-
+    
+    // MARK: - Navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let welcomeVC = segue.destination as? WelcomeViewController
+        welcomeVC?.userName = userName
+    }
+        
+    override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
+        guard loginTextField.text == userName, passwordTextField.text == password else {
+            showErrorAlert(
+                withTitle: "Invalid login or password",
+                andMessage: "Please, enter correct login and password"
+            )
+            return false
+        }
+        return true
+    }
+    
+    // MARK: - IB Actions
+    @IBAction func forgotUserButtonAction(_ sender: UIButton) {
+        showInfoAlert(withTitle: "Login", andMessage: userName)
+    }
+    
+    @IBAction func forgotPassButtonAction(_ sender: UIButton) {
+        showInfoAlert(withTitle: "Password", andMessage: password)
+    }
+    
+    @IBAction func unwind(for segue: UIStoryboardSegue) {
+        loginTextField.text = ""
+        passwordTextField.text = ""
+    }
+    
 // MARK: - Private Methods
-    private func showAlert(withTitle title: String, andMessage message: String) {
+    private func showInfoAlert(withTitle title: String, andMessage message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default)
+        
+        alert.addAction(okAction)
+        present(alert, animated: true)
+    }
+    
+    private func showErrorAlert(withTitle title: String, andMessage message: String) {
         let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
         let okAction = UIAlertAction(title: "OK", style: .default) {_ in
+            self.passwordTextField.text = ""
         }
+        
         alert.addAction(okAction)
         present(alert, animated: true)
     }
