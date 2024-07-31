b76ab447c20d3c32fb94262d328d579520b56f65
Evgeniy <ev.shipitsyn@yandex.ru>
2024-07-31 17:45:48 +0300

    create info view

--- AboutMeApp/ViewControllers/InfoViewController.swift
+++ AboutMeApp/ViewControllers/InfoViewController.swift
@@ -7,23 +7,86 @@
 
 import UIKit
 
-class InfoViewController: UIViewController {
-
+final class InfoViewController: UIViewController {
+    
+    // MARK: - IB Outlets
+    @IBOutlet private var personPhoto: UIImageView!
+    
+    @IBOutlet private var nameLabel: UILabel!
+    @IBOutlet private var surnameLabel: UILabel!
+    @IBOutlet private var ageLabel: UILabel!
+    @IBOutlet private var companyLabel: UILabel!
+    @IBOutlet private var professionLabel: UILabel!
+    
+    // MARK: - Public Properties
+    var userDataInfo: User!
+    
+    // MARK: - View Life Cycles
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.setGradient()
 
-        // Do any additional setup after loading the view.
+        title = userDataInfo.person.fullName
+                
+        personPhoto.layer.cornerRadius = personPhoto.frame.width / 2
+        personPhoto.image = UIImage(named: "photo")
+        personPhoto.contentMode = .scaleAspectFill
+        
+        applyItalicToLabels()
+}
+    
+    // MARK: - Navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let bioVC = segue.destination as? BioViewController
+        bioVC?.userDataBio = userDataInfo
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: - Private Methods
+    private func applyItalicToLabels() {
+        let italicFont = UIFont.italicSystemFont(ofSize: 17)
+        
+        let nameText = "Имя: \(userDataInfo.person.name)"
+        nameLabel.attributedText = makeItalicText(
+            text: nameText,
+            italicFont: italicFont
+        )
+        
+        let surnameText = "Фамилия: \(userDataInfo.person.surname)"
+        surnameLabel.attributedText = makeItalicText(
+            text: surnameText,
+            italicFont: italicFont
+        )
+        
+        let ageText = "Возраст: \(userDataInfo.person.age)"
+        ageLabel.attributedText = makeItalicText(
+            text: ageText,
+            italicFont: italicFont
+        )
+        
+        let companyText = "Компания: \(userDataInfo.person.company.title)"
+        companyLabel.attributedText = makeItalicText(
+            text: companyText,
+            italicFont: italicFont
+        )
+        
+        let professionText = "Профессия: \(userDataInfo.person.company.profession)"
+        professionLabel.attributedText = makeItalicText(
+            text: professionText,
+            italicFont: italicFont
+        )
     }
-    */
-
+    
+    private func makeItalicText(text: String, italicFont: UIFont) -> NSAttributedString {
+        let attributedString = NSMutableAttributedString(string: text)
+        
+        if let colonRange = text.range(of: ":") {
+            let startIndex = text.index(after: colonRange.lowerBound)
+            let range = NSRange(startIndex..., in: text)
+            
+            attributedString.addAttribute(.font, value: italicFont, range: range)
+            }
+            return attributedString
+        }
 }
+
+
